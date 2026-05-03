e202a6807b61115b3f09c5fc4ac9518d515c23cd
rauelon <rauel.anders@web.de>
2026-05-03 18:02:05 +0200

    Create html file with python

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,13 +7,36 @@ def load_data(file_path):
         return json.load(handle)
 
 
-def print_all_animals():
-    """ Prints all animals """
-    animals_data = load_data('animals_data.json')
+def animals_to_string():
+    animals_data = load_data("animals_data.json")
+    animals_html = ""
     for animal in animals_data:
-        print(f"\nName: {animal["name"]}")
-        print(f"Diet: {animal["characteristics"]["diet"]}")
-        print(f"Location: {animal["locations"][0]}")
-        print(f"Type: {animal["characteristics"]["type"]}")
+        name = animal.get("name", "Unknown")
+        diet = animal.get("characteristics", {}).get("diet", "Unknown")
+        animal_type = animal.get("characteristics", {}).get("type", "Unknown")
+        location = animal.get("locations", ["Unknown"])[0]
 
-print_all_animals()
+        animals_html += f"""
+        <li class="cards__item">
+            <div class="card__title">{name}</div>
+            <div class="card__text">
+                Diet: {diet}<br>
+                Location: {location}<br>
+                Type: {animal_type}
+            </div>
+        </li>
+        """
+
+    return animals_html
+
+
+def generate_html():
+    animals_html = animals_to_string()
+    with open("animals_template.html", "r") as file:
+        html_template = file.read()
+    final_html = html_template.replace("{animals}", animals_html)
+    with open("animals.html", "w") as file:
+        file.write(final_html)
+
+
+generate_html()
