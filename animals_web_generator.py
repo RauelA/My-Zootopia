63dcd00e1ba167ce4bff3b87946a9e1e366a9a05
rauelon <rauel.anders@web.de>
2026-05-03 17:42:03 +0200

    Read from json data

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,1 +1,19 @@
+import json
 
+
+def load_data(file_path):
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+def print_all_animals():
+    """ Prints all animals """
+    animals_data = load_data('animals_data.json')
+    for animal in animals_data:
+        print(f"\nName: {animal["name"]}")
+        print(f"Diet: {animal["characteristics"]["diet"]}")
+        print(f"Location: {animal["locations"][0]}")
+        print(f"Type: {animal["characteristics"]["type"]}")
+
+print_all_animals()
